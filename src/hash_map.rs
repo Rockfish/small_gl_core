bc4afd6d1be79f7469ce9f5155c5b9a74630b247
John Hatch <john@rockfishnw.com>
2023-12-13 17:23:12 -0500

    on error retry loading texture with different path

--- src/hash_map.rs
+++ src/hash_map.rs
@@ -1,58 +1,145 @@
-pub use ahash::{AHasher, RandomState};
-pub use hashbrown;
+use glam::Vec2;
+use rand::{thread_rng, Rng};
+// use rand_distr::{Distribution, Normal};
+use crate::error::Error;
+use crate::error::Error::PathError;
+use std::cmp::Ordering;
+use std::ops::Mul;
+use std::path::PathBuf;
 
-// Fast HashMap implementation copied from Bevy crates/bevy_utils/src/lib.rs
-
-use std::{
-    fmt::Debug,
-    hash::{BuildHasher, BuildHasherDefault, Hash},
-};
-
-/// A hasher builder that will create a fixed hasher.
-#[derive(Debug, Clone, Default)]
-pub struct FixedState;
-
-impl std::hash::BuildHasher for FixedState {
-    type Hasher = AHasher;
-
-    #[inline]
-    fn build_hasher(&self) -> AHasher {
-        RandomState::with_seeds(
-            0b10010101111011100000010011000100,
-            0b00000011001001101011001001111000,
-            0b11001111011010110111100010110101,
-            0b00000100001111100011010011010101,
-        )
-        .build_hasher()
+#[inline]
+pub fn min<T: PartialOrd>(a: T, b: T) -> T {
+    if a < b {
+        a
+    } else {
+        b
     }
 }
 
-/// A [`HashMap`][hashbrown::HashMap] implementing aHash, a high
-/// speed keyed hashing algorithm intended for use in in-memory hashmaps.
-///
-/// aHash is designed for performance and is NOT cryptographically secure.
-pub type HashMap<K, V> = hashbrown::HashMap<K, V, BuildHasherDefault<AHasher>>;
+#[inline]
+pub fn max<T: PartialOrd>(a: T, b: T) -> T {
+    if a > b {
+        a
+    } else {
+        b
+    }
+}
 
-/// A stable hash map implementing aHash, a high speed keyed hashing algorithm
-/// intended for use in in-memory hashmaps.
-///
-/// Unlike [`HashMap`] this has an iteration order that only depends on the order
-/// of insertions and deletions and not a random source.
-///
-/// aHash is designed for performance and is NOT cryptographically secure.
-pub type StableHashMap<K, V> = hashbrown::HashMap<K, V, FixedState>;
+pub fn f32_max(a: f32, b: f32) -> f32 {
+    match a.partial_cmp(&b).unwrap() {
+        Ordering::Less => b,
+        Ordering::Equal => b,
+        Ordering::Greater => a,
+    }
+}
 
-/// A [`HashSet`][hashbrown::HashSet] implementing aHash, a high
-/// speed keyed hashing algorithm intended for use in in-memory hashmaps.
-///
-/// aHash is designed for performance and is NOT cryptographically secure.
-pub type HashSet<K> = hashbrown::HashSet<K, BuildHasherDefault<AHasher>>;
+pub fn rand_int(x: i32, y: i32) -> i32 {
+    thread_rng().gen_range(x..=y)
+}
 
-/// A stable hash set implementing aHash, a high speed keyed hashing algorithm
-/// intended for use in in-memory hashmaps.
-///
-/// Unlike [`HashSet`] this has an iteration order that only depends on the order
-/// of insertions and deletions and not a random source.
-///
-/// aHash is designed for performance and is NOT cryptographically secure.
-pub type StableHashSet<K> = hashbrown::HashSet<K, FixedState>;
+pub fn rand_float() -> f32 {
+    thread_rng().gen_range(0.0..=1.0)
+}
+
+pub fn rand_in_range(x: f32, y: f32) -> f32 {
+    thread_rng().gen_range(x..=y)
+}
+
+pub fn rand_bool() -> bool {
+    thread_rng().gen_range(0.0..=1.0) > 0.5
+}
+
+//returns a random float in the range -1 < n < 1
+pub fn random_clamped() -> f32 {
+    thread_rng().gen_range(-1.0..=1.0)
+}
+
+// pub fn rand_normal_distribution() -> f32 {
+//     let normal: Normal<f32> = Normal::new(2.0, 0.2).unwrap();
+//     let v = normal.sample(&mut thread_rng());
+//     v
+// }
+
+pub fn truncate(v: Vec2, max: f32) -> Vec2 {
+    if v.length() > max {
+        let v = v.normalize_or_zero();
+        return v.mul(max);
+    }
+    v
+}
+
+pub fn wrap_around(pos: &mut Vec2, max_x: i32, max_y: i32) {
+    if pos.x > max_x as f32 {
+        pos.x -= max_x as f32;
+    }
+
+    if pos.x < 0.0 {
+        pos.x += max_x as f32;
+    }
+
+    if pos.y < 0.0 {
+        pos.y += max_y as f32;
+    }
+
+    if pos.y > max_y as f32 {
+        pos.y -= max_y as f32
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    // use crate::utils::{rand_normal_distribution, truncate, wrap_around};
+    use crate::utils::{truncate, wrap_around};
+    use glam::vec2;
+
+    #[test]
+    pub fn test_truncate() {
+        let mut v = vec2(100.0, 100.0);
+        println!("length: {}", v.length());
+
+        v = truncate(v, 5.0);
+        println!("vec: {:?}  length: {}", v, v.length());
+    }
+
+    #[test]
+    pub fn test_wraparound() {
+        let mut v = vec2(10.0, 10.0);
+        wrap_around(&mut v, 8, 8);
+        println!("{:?}", v);
+        let mut v = vec2(10.0, 10.0);
+        wrap_around(&mut v, 10, 11);
+        println!("{:?}", v);
+    }
+
+    // #[test]
+    // pub fn test_distribution() {
+    //     for i in 0..1000 {
+    //         let x = rand_normal_distribution() - 2.0;
+    //         println!("{x}");
+    //     }
+    // }
+
+    #[test]
+    pub fn test_projection() {
+        let wave_vec = vec2(10.0, 10.0);
+        let spot_vec = vec2(9.0, 4.0);
+
+        let proj_vec = spot_vec.project_onto(wave_vec);
+
+        println!("{:?}", proj_vec);
+    }
+}
+
+pub fn get_exists_filename(directory: &PathBuf, filename: &str) -> Result<PathBuf, Error> {
+    let path = directory.join(&filename);
+    if path.is_file() {
+        return Ok(path);
+    }
+    let filepath = PathBuf::from(filename.replace("\\", "/"));
+    let filename = filepath.file_name().unwrap();
+    let path = directory.join(&filename);
+    if path.is_file() {
+        return Ok(path);
+    }
+    Err(PathError(format!("filename not found: {:?}", filename.to_os_string())))
+}
